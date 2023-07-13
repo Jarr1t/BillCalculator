d2bf54a9bfab9446b4816c23facadde016a96a3a
Jarrit Alicea <jarr1t@jarrits-mbp.mynetworksettings.com>
2023-07-13 13:38:52 -0400

    started working on camera

--- components/Home.js
+++ components/Home.js
@@ -1,9 +1,19 @@
-import {View, Text } from 'react-native';
+import {View, Text, Button} from 'react-native';
+import { useEffect, useRef, useState } from 'react';
+import { Camera } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
 
 export default function Home() {
+    let cameraRef = useRef();
+    const [hasCameraPermissions, setHasCameraPermissions] = useState(undefined);
+    
+
     return (
         <View>
             <Text>PeepeePoopooPeepee</Text>
+            <Button 
+                title="This is a button"
+            />
         </View>
     )
 };
