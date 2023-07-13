c1ddac16d5d0afb2b5f3ae040692cee87b542a77
Jarrit Alicea <jarr1t@jarrits-mbp.mynetworksettings.com>
2023-07-13 13:52:27 -0400

    app now asks for camera and gallery permissions

--- components/Home.js
+++ components/Home.js
@@ -6,7 +6,16 @@ import * as MediaLibrary from 'expo-media-library';
 export default function Home() {
     let cameraRef = useRef();
     const [hasCameraPermissions, setHasCameraPermissions] = useState(undefined);
-    
+    const [hasMediaLibraryPermissions, setHasMediaLibraryPermissions] = useState(undefined);
+
+    useEffect(() => {
+        (async () => {
+            const cameraPermissions = await Camera.requestCameraPermissionsAsync();
+            const mediaLibraryPermissions = await MediaLibrary.requestPermissionsAsync();
+            setHasCameraPermissions(cameraPermissions.status === "granted")
+            setHasMediaLibraryPermissions(mediaLibraryPermissions.status === "granted")
+        })();
+    }, [])
 
     return (
         <View>
