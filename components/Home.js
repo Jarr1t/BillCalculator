05a3353b83c03b17e5c10460116f7dd708d0aa60
Anne <anne@Annes-MacBook-Pro.local>
2023-07-14 15:56:15 -0400

    Update: updated expo version

--- components/Home.js
+++ components/Home.js
@@ -3,7 +3,7 @@ import {View, Text } from 'react-native';
 export default function Home() {
     return (
         <View>
-            <Text>PeepeePoopooPeepee</Text>
+            <Text>hi</Text>
         </View>
     )
 };
