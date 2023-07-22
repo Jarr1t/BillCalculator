dffb75bbfae64200020cfe50ea9b61dd20fee827
Anne <anne@Annes-MacBook-Pro.local>
2023-07-21 21:06:59 -0400

    item list

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
 import Home from './components/Home';
+import ItemList from './components/ItemList';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-      <Home />
-    </View>
+      {/* <Home /> */}
+      <ItemList />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    height: '100%',
     flex: 1,
-    backgroundColor: '#fff'
   },
 });
